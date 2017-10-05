ff1040b5deaa087216478a44da6a44d7c6c9b33d
vagrant <vagrant@localhost.localdomain>
2017-10-05 05:23:35 +0000

    initial workstation cookbook commit

---
+++ recipes/setupCookBookExample.rb
@@ -0,0 +1,21 @@
+package 'vim-enhanced' do 
+ action :install
+end 
+
+
+package 'tree' do
+  action :install
+end
+
+package 'git' do 
+ action :install
+end 
+
+package 'ntp' do 
+  action :install
+end
+
+file '/etc/modt' do 
+  content 'This is chnaged from setupCookBook.rb while learning chef'
+  action :create
+end 
